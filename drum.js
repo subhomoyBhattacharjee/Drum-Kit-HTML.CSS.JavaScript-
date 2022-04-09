c5d3e903388df86f823f70cf468f76600027c12e
subhomoy <subhomoy85@gmail.com>
2022-04-09 20:47:49 +0530

    'drumkit'

---
+++ drum.js
@@ -0,0 +1,91 @@
+
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons;i++){
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+var buttoninnerHTML = this.innerHTML;
+
+makesound(buttoninnerHTML);
+buttonAnnimation(buttoninnerHTML);
+
+
+}
+);
+}
+
+document.addEventListener("keypress",function(event) {
+	makesound(event.key);
+	buttoninnerHTML(event.key);
+	buttonAnnimation(event.key);
+
+});
+
+
+
+function makesound(key){
+
+	switch (key){
+	case "w":
+	var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case "a":
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case "s":
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case "d":
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    case "j":
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case "k":
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+    case "l":
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+
+     default:console.log(buttoninnerHTML);
+
+     }
+
+}
+
+        function buttonAnnimation(currentkey){
+        	var activeButton = document.querySelectorAll("." + currentkey)
+        	activeButton.classList.add("pressed");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
